eb261d44955f0de4dd005b9dcfb65e0b7817f3e6
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-03-30 13:18:05 +0700

    add default identity

--- src/TheFakeShop.IdentityServer/Startup.cs
+++ src/TheFakeShop.IdentityServer/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheFakeShop.IdentityServer.Contexts;
 
 namespace TheFakeShop.IdentityServer
 {
@@ -23,6 +26,13 @@ namespace TheFakeShop.IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<IdentityServerDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<IdentityServerDbContext>();
+
             services.AddControllersWithViews();
         }
 
