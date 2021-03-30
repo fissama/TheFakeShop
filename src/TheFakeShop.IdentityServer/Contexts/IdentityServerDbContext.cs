78137ccd6cde1abe6a5e1697965f6b88d74f1197
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-03-30 10:38:13 +0700

    Add IdentityServerDbContext

---
+++ src/TheFakeShop.IdentityServer/Contexts/IdentityServerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheFakeShop.IdentityServer.Contexts
+{
+    public class IdentityServerDbContext : DbContext
+    {
+        public IdentityServerDbContext(DbContextOptions<IdentityServerDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
