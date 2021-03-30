59af116fc0d62504abe5cdf3765a14a398f9ac9e
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-03-30 13:20:32 +0700

    Add Site in IdentityServer to edit attribute

---
+++ src/TheFakeShop.IdentityServer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TheFakeShop.IdentityServer.Context;
+
+[assembly: HostingStartup(typeof(TheFakeShop.IdentityServer.Areas.Identity.IdentityHostingStartup))]
+namespace TheFakeShop.IdentityServer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserDbContext>();
+            });
+        }
+    }
+}
