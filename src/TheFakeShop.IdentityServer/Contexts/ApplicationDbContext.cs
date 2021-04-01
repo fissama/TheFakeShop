0a0a6755e3ef8a49a7e0a76ef1b81b6cd240b527
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-01 23:46:47 +0700

    Update ApplicationDbContext

--- src/TheFakeShop.IdentityServer/Contexts/ApplicationDbContext.cs
+++ src/TheFakeShop.IdentityServer/Contexts/ApplicationDbContext.cs
@@ -4,14 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheFakeShop.IdentityServer.Models;
 
 namespace TheFakeShop.IdentityServer.Contexts
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
