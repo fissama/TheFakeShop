877ab9df04d772da102154b0ffdcd2a83e91a6ab
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-02 21:33:41 +0700

    [TheFakeShop.ShareModels] Add sub-project ShareModels

---
+++ src/TheFakeShop.ViewModel/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TheFakeShop.ViewModel
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+        
+        public int? parentId { get; set; }
+    }
+}
