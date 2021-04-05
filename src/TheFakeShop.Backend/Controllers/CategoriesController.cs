e352d7dbc8fe1c8cf3b531ed05fb5f12dc2bb6ff
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-05 17:03:26 +0700

    [TheFakeShop.Backend] Add getID, put, post, delete in CategoryController

--- src/TheFakeShop.Backend/Controllers/CategoriesController.cs
+++ src/TheFakeShop.Backend/Controllers/CategoriesController.cs
@@ -30,5 +30,73 @@ namespace TheFakeShop.Backend.Controllers
                 .Select(x => new CategoryViewModel { Id = x.CategoryId, CategoryName = x.CategoryName, parentId = x.ParentId })
                 .ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<CategoryViewModel>> GetCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var cateVM = new CategoryViewModel
+            {
+                Id = category.CategoryId,
+                CategoryName = category.CategoryName,
+                parentId = category.ParentId
+            };
+
+            return cateVM;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, CategoryPostRequest cateRequest)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            category.CategoryName = cateRequest.Name;
+            category.ParentId = cateRequest.parentId;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CategoryViewModel>> PostCategory(CategoryPostRequest cateRequest)
+        {
+            var category = new Category
+            {
+                CategoryName = cateRequest.Name,
+                ParentId = cateRequest.parentId
+            };
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategory", new { id = category.CategoryId }, new CategoryViewModel { Id = category.CategoryId, CategoryName = category.CategoryName, parentId = category.ParentId });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
