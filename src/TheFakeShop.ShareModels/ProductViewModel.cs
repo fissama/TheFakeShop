4948524c189d726d15e4f8651e91586be6091688
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-06 09:34:32 +0700

    [TheFakeShop.ShareModels] Add ProductViewModel

---
+++ src/TheFakeShop.ShareModels/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheFakeShop.ShareModels
+{
+    class ProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public int? InStock { get; set; }
+
+        public string Description { get; set; }
+
+        public IList<string> ProductImages  { get; set; }
+    }
+}
