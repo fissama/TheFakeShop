28dfcebc600d493ffda9923bbe784c54b77ceb9c
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-07 13:58:20 +0700

    [TheFakeShop.ShareModels] Add RatingCreateRequest

---
+++ src/TheFakeShop.ShareModels/RatingCreateRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFakeShop.ShareModels
+{
+    public class RatingCreateRequest
+    {
+        [Required]
+        public int PratingId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public byte? Rating { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int? ProductID { get; set; }
+    }
+}
