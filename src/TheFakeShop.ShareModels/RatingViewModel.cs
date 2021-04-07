3ec754e4ef8c67ad8dd2655009b56f68ccdc269b
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-07 13:57:26 +0700

    [TheFakeShop.ShareModels] Update RatingViewModel

--- src/TheFakeShop.ShareModels/RatingViewModel.cs
+++ src/TheFakeShop.ShareModels/RatingViewModel.cs
@@ -19,5 +19,7 @@ namespace TheFakeShop.ShareModels
         public string Title { get; set; }
         
         public string Content { get; set; }
+
+        public int? ProductID { get; set; }
     }
 }
