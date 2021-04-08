825bc83c0b621e9ddc27c5daedcdbc8865614c5c
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-08 17:10:32 +0700

    [TheFakeShop.Frontend] Add ProductApiClient

---
+++ src/TheFakeShop.Frontend/Services/ProductApiClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TheFakeShop.ShareModels;
+
+namespace TheFakeShop.Frontend.Services
+{
+    public class ProductApiClient : IProductApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IList<ProductViewModel>> GetProducts()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44358/products");
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsAsync<IList<ProductViewModel>>();
+        }
+    }
+}
