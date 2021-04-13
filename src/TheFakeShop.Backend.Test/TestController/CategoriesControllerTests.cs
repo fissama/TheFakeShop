6cc096bd42e453ab20ace2207332f58bff21efe3
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-13 20:47:01 +0700

    [TheFakeShop.Backend.Test] Modify basic

---
+++ src/TheFakeShop.Backend.Test/TestController/CategoriesControllerTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace TheFakeShop.Backend.Test
+{
+    public class CategoriesControllerTests : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
