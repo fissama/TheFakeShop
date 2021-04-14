43bb21895287a220e7c576b8b1176f6d99551e7a
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-14 23:37:40 +0700

    [TheFakeShop.Backend.Test] Add test CategoryViewComponent

---
+++ src/TheFakeShop.Backend.Test/TestViewComponent/CategoryViewComponentTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheFakeShop.Backend.Controllers;
+using TheFakeShop.Frontend.Services;
+using TheFakeShop.Frontend.ViewComponents;
+using TheFakeShop.ShareModels;
+using Xunit;
+
+namespace TheFakeShop.Backend.Test.TestViewComponent
+{
+    public class CategoryViewComponentTest : IClassFixture<SqliteInMemoryFixture>
+    {
+        private readonly SqliteInMemoryFixture _fixture;
+
+        public CategoryViewComponentTest(SqliteInMemoryFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.CreateDatabase();
+            var dbContext = _fixture.Context;
+            var category = new CategoryPostRequest { Name = "Test category" };
+
+            var controller = new CategoriesController(dbContext);
+            var result = controller.PostCategory(category);
+        }
+
+        [Fact]
+        public async Task PostCategory_Success()
+        {
+            var categoryMenu = new CategoryMenuViewComponent(_fixture.CategoryApiClient);
+            var result = categoryMenu.InvokeAsync();
+            var createdAtActionResult = Assert.IsType<IViewComponentResult>(result);
+          /*  var returnValue = Assert.IsType<CategoryViewModel>(createdAtActionResult.Value);*/
+           // Assert.Equal();
+        }
+    }
+}
