d85c83a6c3a18639a7e2c2d6a67b133c22ffe2ad
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-21 22:18:54 +0700

    complete demo react-admin

--- the-fake-admin/client/src/App.js
+++ the-fake-admin/client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {Admin, Resource} from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+import PostList from './components/PostList'
+import PostCreate from './components/PostCreate'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return  <Admin dataProvider={restProvider('http://localhost:3000')}>
+    <Resource name='posts' list={PostList} create={PostCreate}/>
+  </Admin>
 }
 
 export default App;
