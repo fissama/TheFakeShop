34552323d55f9523d6be0924d69e8cac2a90e4ef
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-22 14:57:10 +0700

    add react-router-dom

--- the-fake-shop.admin-site/src/App.js
+++ the-fake-shop.admin-site/src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
-import ListCategory from "./components/ListCategory";
+import React from "react";
+import ListCategory from "./components/Category/ListCategory";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { LIST_CATEGORY } from "./constants/page";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <ListCategory />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route to={LIST_CATEGORY}>
+          <ListCategory />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
