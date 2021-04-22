bcb3c264722f7f21a03695fc7a24fb1cb2ecc91d
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-22 15:09:31 +0700

    add constants /page

---
+++ the-fake-shop.admin-site/src/constants/page.js
@@ -0,0 +1,7 @@
+export const LIST_PRODUCT = 'product';
+export const CREATE_PRODUCT = 'product';
+export const UPDATE_PRODUCT = 'product';
+
+export const LIST_CATEGORY = 'category';
+export const CREATE_CATEGORY = 'category';
+export const UPDATE_CATEGORY = 'category';
