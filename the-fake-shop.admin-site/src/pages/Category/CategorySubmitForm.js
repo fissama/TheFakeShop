f9aac01e56c018b8e89fb1ad05080a0030914931
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-23 14:56:18 +0700

    remove unnecessery code

--- the-fake-shop.admin-site/src/pages/Category/CategorySubmitForm.js
+++ the-fake-shop.admin-site/src/pages/Category/CategorySubmitForm.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import { withRouter } from "react-router-dom";
 import CategoryService from "../../services/CategoryService";
-import { LIST_CATEGORY } from '../../constants/page';
-import { Link } from 'react-router-dom';
 import history from '../../helpers/history';
 
 
