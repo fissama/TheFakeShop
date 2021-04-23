7602897b1c63ca87c43222526d32d0f80f6bce0c
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-23 11:23:05 +0700

    Add create and edit category

--- the-fake-shop.admin-site/src/components/Category/CategorySubmitForm.js
+++ the-fake-shop.admin-site/src/components/Category/CategorySubmitForm.js
@@ -1,53 +1,74 @@
 import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
-import { withRouter,useParams } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import CategoryService from "../../services/CategoryService";
 
-const CategorySubmitForm = ({match}) => {
+const CategorySubmitForm = ({ match }) => {
+  const [categoryId,setCategoryId] = useState(match.params.id);
   const [Category, setCategory] = useState({});
   const formik = useFormik({
+    enableReinitialize : true,
     initialValues: {
-      CategoryName: '',
-      ParentId: ''
+      name: Category.categoryName,
+      parentId: Category.parentId
     },
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
+      let result = window.confirm("Are you sure?");
+      console.log(values);
+      if (result) {
+        let isCreate = categoryId===undefined?true:false;
+        console.log(isCreate);
+        if(isCreate){
+          CategoryService.create(values);
+        }
+        else {
+          CategoryService.edit(categoryId,values);
+        }
+      }
+    }
   });
 
   useEffect(() => {
-    fetchCategory(match.params.id);
-    formik.values.CategoryName=Category.categoryName;
-    formik.values.ParentId=Category.parentId;
+    async function fetchdate(){
+    setCategoryId(match.params.id);
+    console.log(categoryId);
+    if (categoryId !== undefined) {
+      await fetchCategory(categoryId);
+      console.log(formik.initialValues);
+    } else {
+    }
+    
+  }
+  fetchdate();
   }, [match.params.id]);
+
+  const fetchCategory = async (itemId) => {
+    console.log(CategoryService.get(itemId));
+    setCategory(await (await CategoryService.get(itemId)).data)
+  };
   console.log(Category);
 
-  const fetchCategory = (itemId) => {
-    if(itemId !== undefined){
-    CategoryService.get(itemId).then(({ data }) => {
-      setCategory(data);
-    });
-  }
-  };
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="CategoryName">Category Name</label>
+      <label htmlFor="name">Category Name</label>
       <input
-        id="CategoryName"
-        name="CategoryName"
+        id="name"
+        name="name"
         type="text"
         onChange={formik.handleChange}
-        value={formik.values.CategoryName}
+        value={formik.values.name}
       />
-      <label htmlFor="ParentId">Parent Id</label>
+      <label htmlFor="parentId">Parent Id</label>
       <input
-        id="ParentId"
-        name="ParentId"
+        id="parentId"
+        name="parentId"
         type="number"
         onChange={formik.handleChange}
-        value={formik.values.ParentId}
+        value={Category.parentId}
       />
-      <button type="submit">Submit</button>
+      <button type="submit">
+        Submit
+      </button>
     </form>
   );
 };
