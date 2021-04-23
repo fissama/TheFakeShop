03d28f9ba1f28cce16fb52a347b0701a4cef5fa0
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-23 09:30:11 +0700

    Update path

--- the-fake-shop.admin-site/src/constants/page.js
+++ the-fake-shop.admin-site/src/constants/page.js
@@ -1,7 +1,5 @@
 export const LIST_PRODUCT = '/product';
-export const CREATE_PRODUCT = '/modifiedCategory';
-export const UPDATE_PRODUCT = 'product';
+export const MODIFIED_PRODUCT = '/modifiedProduct/:id';
 
 export const LIST_CATEGORY = '/category';
-export const CREATE_CATEGORY = '/modifiedCategory/:id';
-export const UPDATE_CATEGORY = 'modifiedCategory';
+export const MODIFIED_CATEGORY = '/modifiedCategory/:id';
