3a67be7537f6cbb4cc5e94ae20ee1eaac3870dc2
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-23 11:22:30 +0700

    Add create_route

--- the-fake-shop.admin-site/src/App.js
+++ the-fake-shop.admin-site/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ListCategory from "./components/Category/ListCategory";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-import { LIST_CATEGORY, CREATE_CATEGORY } from "./constants/page";
+import { LIST_CATEGORY, MODIFIED_CATEGORY, CREATE_CATEGORY } from "./constants/page";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CategorySubmitForm from "./components/Category/CategorySubmitForm";
 
@@ -12,8 +12,10 @@ function App() {
         <Route path={LIST_CATEGORY}>
           <ListCategory />
         </Route>
+        <Route path={MODIFIED_CATEGORY}>
+          <CategorySubmitForm />
+        </Route>
         <Route path={CREATE_CATEGORY}>
-          {console.log("lolo")}
           <CategorySubmitForm />
         </Route>
       </Switch>
--- the-fake-shop.admin-site/src/constants/page.js
+++ the-fake-shop.admin-site/src/constants/page.js
@@ -1,5 +1,8 @@
 export const LIST_PRODUCT = '/product';
 export const MODIFIED_PRODUCT = '/modifiedProduct/:id';
+export const CREATE_PRODUCT = '/modifiedProduct/';
+
 
 export const LIST_CATEGORY = '/category';
 export const MODIFIED_CATEGORY = '/modifiedCategory/:id';
+export const CREATE_CATEGORY = '/modifiedCategory/';
