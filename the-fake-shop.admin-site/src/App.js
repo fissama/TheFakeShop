1b29943cc8b1865ff3fa9f0c047e580335b43763
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-24 21:20:57 +0700

    Add sidebar and header

--- the-fake-shop.admin-site/src/App.js
+++ the-fake-shop.admin-site/src/App.js
@@ -1,49 +1,23 @@
 import React from "react";
-import ListCategory from "./pages/Category/ListCategory";
-import ListUser from "./pages/User/ListUser";
-import ListProduct from "./pages/Product/ListProduct";
-import { Route, Router, Switch } from "react-router-dom";
-import {
-  LIST_CATEGORY,
-  MODIFIED_CATEGORY,
-  CREATE_CATEGORY,
-  LIST_USER,
-  LIST_PRODUCT,
-  MODIFIED_PRODUCT,
-  CREATE_PRODUCT,
-} from "./constants/page";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CategorySubmitForm from "./pages/Category/CategorySubmitForm";
-import ProductSubmitForm from "./pages/Product/ProductSubmitForm";
-import history from "./helpers/history";
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/umd/popper.min.js";
+import $ from "jquery";
+import "./App.css";
+import Home from "./pages/home";
 
 function App() {
+  React.useEffect(() => {
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }, []);
+
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path={LIST_CATEGORY}>
-          <ListCategory />
-        </Route>
-        <Route path={MODIFIED_CATEGORY}>
-          <CategorySubmitForm />
-        </Route>
-        <Route path={CREATE_CATEGORY}>
-          <CategorySubmitForm />
-        </Route>
-        <Route path={LIST_USER}>
-          <ListUser />
-        </Route>
-        <Route path={LIST_PRODUCT}>
-          <ListProduct />
-        </Route>
-        <Route path={MODIFIED_PRODUCT}>
-          <ProductSubmitForm />
-        </Route>
-        <Route path={CREATE_PRODUCT}>
-          <ProductSubmitForm />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+        <Home />
+    </div>
   );
 }
 
