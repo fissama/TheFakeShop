6989606abec2a9c8dfed2b073d15d6b7fd20918b
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-04-30 23:08:01 +0700

    Add cart to customerSite

---
+++ src/TheFakeShop.ShareModels/CartItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFakeShop.ShareModels
+{
+    public class CartItemViewModel
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Qty { get; set; }
+
+        public string Image { get; set; }
+    }
+}
