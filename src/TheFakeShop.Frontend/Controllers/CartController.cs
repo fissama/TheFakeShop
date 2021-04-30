e46ee1f9a32ff1711a2f90db27bb7b1d89412bef
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-05-01 02:45:19 +0700

    Add cart controller

---
+++ src/TheFakeShop.Frontend/Controllers/CartController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheFakeShop.Frontend.Services;
+using TheFakeShop.ShareModels;
+
+namespace TheFakeShop.Frontend.Controllers
+{
+    public class CartController : Controller
+    {
+        [Route("/cart")]
+        public IActionResult Index()
+        {
+            var result = HttpContext.Session.Get<List<CartItemViewModel>>("UserCart");
+            if (result == null)
+            {
+                result = new List<CartItemViewModel>();
+            }
+            return View(result);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            List<CartItemViewModel> cart = HttpContext.Session.Get<List<CartItemViewModel>>("UserCart");
+
+            var deleteMe = cart.Find(x => x.ProductId == id);
+
+            cart.Remove(deleteMe);
+            HttpContext.Session.Set("UserCart", cart);
+            return RedirectToAction("Index","Cart");
+        }
+    }
+}
