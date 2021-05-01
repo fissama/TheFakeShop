639e7dee9426e28d7b2a48ddf8494599521b6b47
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-05-01 17:05:57 +0700

    Add OrderViewModel

---
+++ src/TheFakeShop.ShareModels/OrderCreateRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFakeShop.ShareModels
+{
+    public class OrderCreateRequest
+    {
+        [Required]
+        public string CustomerEmail { get; set; }
+
+        public string Phone { get; set; }
+
+        public decimal? Cost { get; set; }
+
+        public string FullAddress { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string OrderStatus { get; set; }
+
+        public IList<OrderDetailViewModel> orderDetail { get; set; }
+    }
+}
---
+++ src/TheFakeShop.ShareModels/OrderDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFakeShop.ShareModels
+{
+    public class OrderDetailViewModel
+    {
+        public int? Qty { get; set; }
+        public int? ProductId { get; set; }
+        public int? OrderId { get; set; }
+    }
+}
