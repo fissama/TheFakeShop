d08b3068d12dd00096127d7ca1aa336d1981aa42
DESKTOP-KHES07J\Anh Phi <centaurging99@gmail.com>
2021-05-04 14:39:41 +0700

    Remove hardcode in react site

--- the-fake-shop.admin-site/src/constants/connection.js
+++ the-fake-shop.admin-site/src/constants/connection.js
@@ -1,7 +1,6 @@
-export const BACKEND_URL = 'https://localhost:44358';
-export const IDENTITY_SERVER_URL = 'https://localhost:44334';
-export const ADMIN_URL = 'https://localhost:3000/';
+export const BACKEND_URL = process.env.REACT_APP_API;
+export const IDENTITY_SERVER_URL = process.env.REACT_APP_IDENTITY_SERVER_URL;
+export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL;
 
 export const AUTH0_DOMAIN='tefos.jp.auth0.com';
 export const AUTH0_CLIENT_ID='vCPMW3DbXldHAi4tlPHYxnkUhRq8v7XA';
-
