c1ab5936dd158da827a1bb89b9e3864768879f88
phileas <centaurging99@gmail.com>
2022-09-03 22:23:22 +0700

    feat: init ProductService

---
+++ src/TheFakeShop.Service.Product/Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace TheFakeShop.Service.Product.Services
+{
+    public class CategoryService : Category.CategoryBase
+    {
+        public override Task<GetAllCategoryResponse> GetAllCategory(Empty request, ServerCallContext context)
+        {
+            var response = new GetAllCategoryResponse();
+            response.Categories.Add(new GetCategoryResponse { CategoryId = 1, CategoryName = "demo", CreatedAt = Timestamp.FromDateTime(DateTime.Now) });
+            return Task.FromResult(response);
+        }
+
+        public override Task<GetCategoryResponse> GetCategory(GetCategoryRequest request, ServerCallContext context)
+        {
+            var category = new GetCategoryResponse { CategoryId=1,CategoryName="demo",CreatedAt=Timestamp.FromDateTime(DateTime.Now)};
+            
+            return Task.FromResult(category);
+        }
+    }
+}
